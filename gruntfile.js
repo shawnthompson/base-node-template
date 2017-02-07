4a018ee30af52b2084924ac0a1eeb51741978ab5
Shawn Thompson <plansmash@me.com>
2017-02-06 22:29:03 -0500

    adds menu

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 
 				files : [{
 					src : './components/sass/styles.scss',
-					dest : './components/css/style.css'
+					dest : './builds/development/css/style.css'
 				}]
 			}
 		},
